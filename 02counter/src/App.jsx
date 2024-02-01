ff43f8f85c4e8bedfb3f043ea6b45ef17f7b4b6d
Ravi Shankar <ravishankar.has@gmail.com>
2024-02-01 11:11:26 +0530

    some addition in code

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -37,9 +37,10 @@ function App() {
     <h1>React !!!</h1>
     <h2>Counter value: {counter}</h2>
 
-    <button onClick={addValue}>Add value</button>
-    <br />
+    <button onClick={addValue}>Add value</button> {" "}
+    
     <button onClick={removeValue}>Remove value</button>
+    <p>footer :{counter}</p>
     </>
   )
 }
