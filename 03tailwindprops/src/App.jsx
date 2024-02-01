246f586dd5a06718c02b2ee652ca5bc3feec843e
Ravi Shankar <ravishankar.has@gmail.com>
2024-02-01 15:31:05 +0530

    some addition

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1 className='text-3xl bg-purple-500 p-3 rounded-md'>vite with tailwind</h1>
+      <Card></Card>
     </>
   )
 }
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <img src="https://media.gettyimages.com/id/1795115034/photo/semi-final-icc-mens-cricket-world-cup-india-2023.jpg?s=1024x1024&w=gi&k=20&c=GyAS7g0LbISxtJzeIwCSSka91QhY8J1g2hRAvex6TnU=" alt="" />
+        <h1 className='text-2xl bg-purple-500 p-3 rounded'> A card for photos</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quidem?</p>
+    </div>
+  )
+}
+
+export default Card
