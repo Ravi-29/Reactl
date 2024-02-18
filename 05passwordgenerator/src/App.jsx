cecc53dbfd3858eca4dfd323ab2faf86849e775d
Ravi Shankar <ravishankar.has@gmail.com>
2024-02-18 23:04:42 +0530

    .

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -4,8 +4,8 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 
 function App() {
   const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false)
+  const [numberAllowed, setNumberAllowed] = useState();
+  const [charAllowed, setCharAllowed] = useState()
   const [password, setPassword] = useState("")
 
   //useRef hook
