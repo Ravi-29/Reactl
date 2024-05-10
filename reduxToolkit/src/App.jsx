ebb1665aead08a9973f123e785cd46e7d330a271
Ravi Shankar <ravishankar.has@gmail.com>
2024-05-10 11:54:09 +0530

    Redux ToolKit

--- reduxToolkit/src/App.jsx
+++ reduxToolkit/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Addtodo from './components/Addtodo'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <h1>Learn About Redux toolKit</h1>
+      <Addtodo />
+      <Todos/>
     </>
   )
 }
